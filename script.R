7e0c43792a793ec259bdc9f84eab7526bc18b8de
MoDubs11 <william2@tcd.ie>
2020-01-08 13:39:02 -0800

    tutorial update

--- script.R
+++ script.R
@@ -12,3 +12,15 @@ foot_path = rbind(Attack, Defense, Success)
 colnames(foot_path) = rownames(foot_path)
 #A one is used when latent feeds in \
 innerplot(foot_path)
+
+
+# define list of indicators: what variables are associated with
+# what latent variables
+foot_blocks = list(1:4, 5:8, 9:12)
+
+# all latent variables are measured in a reflective way
+foot_modes = c("A", "A", "A")
+
+foot_pls = plspm(spainfoot, foot_path, foot_blocks, modes = foot_modes)
+# path coefficients
+foot_pls$path_coefs
